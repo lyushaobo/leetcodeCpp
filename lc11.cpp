f3680099228785f7ef5691277d4a6df3be5faa17
shaobo <shaobolyu2001@gmail.com>
2024-09-01 12:12:59 +0800

    11. 盛最多水的容器

--- lc11.cpp
+++ lc11.cpp
@@ -3,18 +3,24 @@
 using namespace std;
 
 int main() {
-    vector<int> nums;
+    vector<int> height;
     int n = 1;
     while (n < 10000) { // 注意 while 处理多个 case
         cin >> n;
-        nums.resize(n);
+        height.resize(n);
         for (int i = 0; i < n; i++) {
-            cin >> nums[i]; //将输入值存入容器
+            cin >> height[i]; //将输入值存入容器
         }
         int leftPtr = 0, rightPtr = n-1;
         int ans = 0;
         while (leftPtr < rightPtr) {
-            ans = max(ans, min(nums[leftPtr], nums[rightPtr]));
+            int minVal;
+            if (height[leftPtr] > height[rightPtr]) {
+                minVal = height[rightPtr--];
+            } else {
+                minVal = height[leftPtr++];
+            }
+            ans = max(ans, minVal * (rightPtr - leftPtr + 1));
         }
         for (int i = 0; i < n; i++)
         {
