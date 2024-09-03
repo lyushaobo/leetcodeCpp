1be71591e1af5ca17229e3de0ea4287809c48e39
shaobo <shaobolyu2001@gmail.com>
2024-09-03 23:43:34 +0800

    142. 环形链表II

---
+++ lc142.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+         ListNode *slow = head, *fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (fast == slow) {
+                while (slow != head) {
+                    slow = slow->next;
+                    head = head->next;
+                }
+                return slow;
+            }
+        }
+        return nullptr;
+    }
+};
